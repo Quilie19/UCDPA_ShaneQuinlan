c5e5cde573a085cb7b8ed608e0e31e1abc29a3ec
shane <squilie19@gmail.com>
2021-09-21 20:28:50 +0100

    Merging table columns and joining table rows

--- Analysing_data.py
+++ Analysing_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 football_data = pd.read_csv("football_teams.csv")
 
@@ -35,6 +36,37 @@ df = pd.DataFrame(football_data.drop_duplicates())
 print (df)
 
 print(df.sort_values(by = "Goals", ascending = False).head(10))
+
+premierleague_standings = pd.read_csv("PremierLeague_standings.csv")
+
+print (premierleague_standings)
+
+df1 = pd.DataFrame(premierleague_standings)
+
+print(df.loc[df["Tournament"] == "Premier League"])
+
+print(pd.merge(df,df1,on="Team"))
+
+bundesliga_standings = pd.read_csv("Bundesliga_standings.csv")
+laliga_standings = pd.read_csv("LaLiga_standings.csv")
+ligue1_standings = pd.read_csv("Ligue1_standings.csv")
+seriea_standings = pd.read_csv("SerieA_standings.csv")
+
+df2 = pd.DataFrame(bundesliga_standings)
+df3 = pd.DataFrame(laliga_standings)
+df4 = pd.DataFrame(ligue1_standings)
+df5 = pd.DataFrame(seriea_standings)
+
+concat_df = pd.concat([df1, df2, df3, df4, df5], axis = 0, ignore_index= True)
+print(concat_df)
+
+print(concat_df.sort_values(by = "P", ascending = False).head(10))
+
+
+
+
+
+
 
 
 
